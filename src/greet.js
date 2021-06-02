0e64969c17b1eabbf4bf3e9a7ef503439491eb8e
Mthobisi-za <mthobisimtho333@gmail.com>
2021-06-02 22:29:51 +1400

    updated reset

--- src/greet.js
+++ src/greet.js
@@ -1,60 +1,69 @@
-function makeChanges(){
+function makeChanges() {
     ///global values
     var arg = []
     var name;
     var lang;
     //global values
     ///collect user input
-    function setUserName(str){
+    function setUserName(str) {
         var checkNum = /\d/;
-        if(checkNum.test(str) && str !== ""){
+        if (checkNum.test(str) && str !== "") {
             return "Please input text that has alphabetic values only."
-        } else{
+        } else {
             name = str.toLowerCase()
             return str.toLowerCase()
-            }
+        }
     }
-    function setUserLang(str){
-        if(str == "Isizulu" || str == "isizulu"){
+
+    function setUserLang(str) {
+        if (str == "Isizulu" || str == "isizulu") {
             return lang = "Saw'bona"
-        } else if(str == "English" || str == "english"){
+        } else if (str == "English" || str == "english") {
             return lang = "Hello"
-        } else{
+        } else {
             return lang = "Dumela"
         }
     }
     //collect user inputs
     //store the values to local storage
-    function storeNames(){
+    function storeNames() {
         var local = localStorage.getItem("names");
         //reference local storage
-        if(local == null){
+        if (local == null) {
             //does not exist and local storage is not declared
             arg.push(name)
-             localStorage.setItem("names", arg)
+            localStorage.setItem("names", arg)
             return localStorage.getItem("names")
-        }  else if(localStorage.getItem("names").indexOf(name) !== -1){
+        } else if (localStorage.getItem("names").indexOf(name) !== -1) {
             ///already exist
             return "already exist"
-        }else{
+        } else {
             //add the arrays and assign new values
             var df = [localStorage.getItem("names")];
             df.push(name)
-             localStorage.setItem("names", df);
-             return localStorage.getItem("names")
+            localStorage.setItem("names", df);
+            return localStorage.getItem("names")
         }
     }
     //store the values to local storage
     ///get the counter
-    function getCounter(){
+    function getCounter() {
         return localStorage.getItem("names").split(',').length;
     }
     ///get the counter
+    //reset the data
+    function resetData() {
+        localStorage.clear()
+        arg = []
+        return localStorage.getItem("names")
+    }
+    //reset the data
     ////return an objects factory functions
-    return{
+    return {
         setUserName,
         setUserLang,
         storeNames,
-        getCounter
+        getCounter,
+        resetData
     }
 }
